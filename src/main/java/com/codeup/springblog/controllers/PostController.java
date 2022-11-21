71783bb1cd60c14159821d7c9ffd4306510608f6
tesfalem tekie <teshanna72016@gmail.com>
2022-11-21 05:58:56 -0600

    repositories-and-jpa exercise in progress

--- src/main/java/com/codeup/springblog/controllers/PostController.java
+++ src/main/java/com/codeup/springblog/controllers/PostController.java
@@ -21,10 +21,12 @@ public class PostController {
 
     @GetMapping
     public String allPosts(Model model) {
-        Post post1 = new Post(1, "First", "This is my first post!");
-        Post post2 = new Post(2, "Second", "Hey everyone, I'm back");
-        List<Post> allPosts = new ArrayList<>(List.of(post1, post2));
-        model.addAttribute("allPosts", allPosts);
+//        Post post1 = new Post(1, "First", "This is my first post!");
+//        Post post2 = new Post(2, "Second", "Hey everyone, I'm back");
+//        List<Post> allPosts = new ArrayList<>(List.of(post1, post2));
+//        model.addAttribute("allPosts", allPosts);
+        List<Post> posts = postDao.findAll();
+        model.addAttribute("posts", posts);
         return "/posts/index";
     }
 
@@ -45,14 +47,15 @@ public class PostController {
     }
 
     @GetMapping("/create")
-    public String createPost() {
+    public String createNewPost() {
         return "/posts/create";
     }
 
-    @GetMapping("/posts")
-    @ResponseBody
-    public String viewPost() {
-        return "view the form for creating a post!";
+    @PostMapping("/create")
+    public String addNewPost(@RequestParam (name="title") String title, @RequestParam (name="body") String body) {
+        Post post = new Post(title, body);
+        postDao.save(post);
+        return "redirect:/posts/index";
     }
 
     @PostMapping("/create")
